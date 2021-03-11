37f38a99893bbdd636ca928ae4edf7e8430cd46f
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2021-03-11 12:53:03 +0300

    Check for uniqueness for ids

---
+++ order_dispenser.py
@@ -0,0 +1,116 @@
+from valdec.errors import ValidationArgumentsError as ValidationError
+from useful_functions import find_by
+from courier import Courier
+from order import Order
+
+
+class OrderDispenser:
+    def __init__(self):
+        self.couriers = []
+        self.unactive_orders = []
+
+    def add_new_couriers(self, couriers_json: dict) -> (bool, dict):
+        data = couriers_json['data']
+        failed_ids = []
+        success_ids = []
+
+        for courier in data:
+            try:
+                id = courier['courier_id']
+
+                find_same_id = False
+                # проходим по всем уже записанным курьерам и
+                # проверям уникальность нового id
+                for c in self.couriers:  
+                    find_same_id = find_same_id or c.id == id
+
+                if find_same_id:
+                    raise ValueError('Not unique id')
+                # 
+                type = courier['courier_type']
+                regions = courier['regions']
+                working_hours = courier['working_hours']
+                new_courier = Courier(id, type, regions, working_hours)
+            except (
+                KeyError,  # если нету какого-то поля в json
+                ValidationError,  # если какое-то значение не прошло валидацию по типу
+                ValueError  # если какое-то значение не прошло валидацию по значению
+            ):
+                failed_ids.append(id)
+            except Exception as unknown_exception:
+                failed_ids.append(id)
+                print('Unchecked Error:', unknown_exception)
+            else:
+                self.couriers.append(new_courier)
+                success_ids.append(id)
+        
+        if failed_ids:  # есть провальные
+            return False, {
+                "validation_error": {
+                    "couriers": [{'id':id} for id in failed_ids]
+                }
+            }
+        else:
+            return True, {
+                "couriers": [{'id': id} for id in success_ids]
+            }
+
+    def patch_courier(self, id, patched_data: dict) -> (bool, dict):
+        try:
+            index = find_by(self.couriers, lambda courier: courier.id == id)
+            type = patched_data.get('courier_type')
+            regions = patched_data.get('regions')
+            working_hours = patched_data.get('working_hours')
+            if type:
+                self.couriers[index].courier_type = type
+            if regions:
+                self.couriers[index].regions = regions
+            if working_hours:
+                self.couriers[index].working_hours = working_hours
+        except (AssertionError, ValueError):
+            return False, {}
+        else:
+            return True, self.couriers[index].to_dict()
+
+    def add_new_orders(self, orders_json: dict) -> (bool, dict):
+        data = orders_json['data']
+        failed_ids = []
+        success_ids = []
+
+        for order in data:
+            try:
+                id = order['order_id']
+                find_same_id = False
+                for o in self.unactive_orders:
+                    find_same_id = find_same_id or o.id == id
+                
+                if find_same_id:
+                    raise ValueError('Not unique id')
+
+                weight = order['weight']
+                region = order['region']
+                delivery_hours = order['delivery_hours']
+                new_order = Order(id, weight, region, delivery_hours)
+            except (
+                KeyError,  # если нету какого-то поля в json
+                ValidationError,  # если какое-то значение не прошло валидацию по типу
+                ValueError  # если какое-то значение не прошло валидацию по значению
+            ):
+                failed_ids.append(id)
+            except Exception as unknown_exception:
+                failed_ids.append(id)
+                print('Unchecked Error:', unknown_exception)
+            else:
+                self.unactive_orders.append(new_order)
+                success_ids.append(id)
+        
+        if failed_ids:  # есть провальные
+            return False, {
+                "validation_error": {
+                    "orders": [{'id':id} for id in failed_ids]
+                }
+            }
+        else:
+            return True, {
+                "orders": [{'id': id} for id in success_ids]
+            }
