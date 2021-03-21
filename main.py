8e10894716eb3ac493158daa22245ca6476fdc4e
Dartt0n <antonkudryavtsevdoem@gmail.com>
2021-03-21 18:20:56 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ with app.app_context():
     database.create_all()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
